4f9fe4b71c7e7799747fb6a324e1c4685f5ad4d6
Heliflyer <web_registrations@hotmail.com>
2022-07-22 22:17:50 +0100

    Why does this not work?

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Hello World! I am alive and gay</h1>")
+		fmt.Fprint(w, "<p>I live to love</p>")
 	})
 
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
