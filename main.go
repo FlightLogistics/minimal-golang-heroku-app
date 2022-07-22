bd38505d56ebda2952eb437092d2babe68613f57
Heliflyer <web_registrations@hotmail.com>
2022-07-22 22:23:12 +0100

    I reconnected

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Hello World! I am alive and gay</h1>")
-		fmt.Fprint(w, "<p>I live to love and to be gay</p>")
 	})
 
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
